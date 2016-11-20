e3f49edab66edf0c4f77fe2f28041a3ae513dd22
Canna Wen <cannawen@gmail.com>
2016-11-20 15:30:43 -0500

    uploads

--- src/sculpture/core.cljs
+++ src/sculpture/core.cljs
@@ -4,11 +4,40 @@
 
 (enable-console-print!)
 
-(defn appview []
-  [:input {:type "file" :multiple true}])
+(defonce state (r/atom {:photos []}))
+
+(defn photo-view [file]
+  (let [data-url (r/atom nil)
+        reader (when-not @data-url
+                 (doto (js/FileReader.)
+                   (aset "onload" (fn [e]
+                                    (reset! data-url (.. e -target -result))))
+                   (.readAsDataURL file)))]
+    (fn []
+      [:div
+        (.-name file)
+        (when @data-url
+          [:img {:src @data-url
+                 :height "100px"}])])))
+
+(defn app-view []
+  [:div
+   [:input {:type      "file"
+            :multiple  true
+            :on-change (fn [e]
+                         (let [file-list (.. e -target -files)
+                               files (map (fn [n]
+                                            (aget file-list n))
+                                          (range (.-length file-list)))]
+                           (swap! state assoc :photos files)))}]
+   (let [photos (@state :photos)]
+     (for [photo photos]
+       ^{:key (.-name photo)}
+       [photo-view photo]))])
+
 
 (defn render []
-  (r/render-component [appview] (.. js/document (getElementById "app"))))
+  (r/render-component [app-view] (.. js/document (getElementById "app"))))
 
 (defn init []
   (render))
--- test/sculpture/test/file_upload.clj
+++ test/sculpture/test/file_upload.clj
@@ -1,13 +1,32 @@
 (ns sculpture.test.file-upload
   (:require
     [clojure.test :refer [deftest testing is]]
-    [clj-webdriver.taxi :as taxi]))
+    [clj-webdriver.taxi :as taxi])
+  (:import
+    [org.openqa.selenium.phantomjs.PhantomJSDriver]))
 
-(taxi/set-driver! {:browser :phantomjs})
 
-(deftest hello-world
-  (taxi/to "http://localhost:3449")
-  (let [file-input (taxi/find-element {:tag :input
-                                       :type "file"
-                                       :multiple ""})]
-    (is file-input)))
+(def driver
+  (taxi/set-driver! {:browser :phantomjs}))
+
+(deftest uploading-photos
+  (testing "when page loads"
+    (taxi/to "http://localhost:3449")
+    (testing "then input exists"
+      (let [file-input (taxi/find-element {:tag :input
+                                           :type "file"
+                                           :multiple ""})]
+        (is file-input)
+
+        (testing "when uploading file"
+          (spit "/tmp/photo.png" "...")
+          (taxi/execute-script "document.getElementsByTagName('input')[0].removeAttribute('multiple')")
+          (taxi/send-keys file-input "/tmp/photo.png")
+          ;(println "Driver" (.executePhantomJS (:webdriver driver) "console.log('hello')"))
+          (println "Driver" (type (:webdriver driver)))
+         (testing "then show file name on page"
+            (is (taxi/find-element {:tag :div
+                                    :text "C:\\fakepath\\photo.png"}))))
+        (testing "when uploading multiple files"
+          (spit "/tmp/photo1.png" "...")
+          (spit "/tmp/photo2.png" "..."))))))
