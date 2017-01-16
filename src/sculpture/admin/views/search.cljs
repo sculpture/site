4956bdd764e239386ffb12df11fad1977756d0b3
Rafal Dittwald <rafal.dittwald@gmail.com>
2017-01-16 11:29:57 -0500

    Refactor search-view - split into query- and results- views

--- src/sculpture/admin/views/search.cljs
+++ src/sculpture/admin/views/search.cljs
@@ -4,22 +4,27 @@
     [sculpture.admin.state.core :refer [subscribe dispatch!]]
     [sculpture.admin.views.search-result :refer [search-result-view]]))
 
+(defn query-view []
+  [:div.query
+   [:input {:value @(subscribe [:query])
+            :on-change (fn [e]
+                         (dispatch! [:set-query (.. e -target -value)]))}]])
+
+(defn results-view []
+  [:div.results
+   (let [results (subscribe [:results])
+         grouped-results (group-by :type @results)]
+     (for [[type results] grouped-results]
+       ^{:key type}
+       [:div.group {:class (str type)}
+        [:h2 (-> (str type)
+                 (string/capitalize)
+                 (str "s"))]
+        (for [result results]
+          ^{:key (result :id)}
+          [search-result-view result])]))])
+
 (defn search-view []
-  (let [query (subscribe [:query])
-        results (subscribe [:results])]
-    [:div.search
-     [:div.query
-      [:input {:value @query
-               :on-change (fn [e]
-                            (dispatch! [:set-query (.. e -target -value)]))}]]
-     [:div.results
-      (let [grouped-results (group-by :type @results)]
-        (for [[type results] grouped-results]
-          ^{:key type}
-          [:div.group {:class (str type)}
-           [:h2 (-> (str type)
-                    (string/capitalize)
-                    (str "s"))]
-           (for [result results]
-             ^{:key (result :id)}
-             [search-result-view result])]))]]))
+  [:div.search
+   [query-view]
+   [results-view]])
