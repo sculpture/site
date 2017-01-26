78ff955bee15dfac39920f77ebe05527979c1959
Rafal Dittwald <rafal.dittwald@gmail.com>
2017-01-25 19:53:45 -0500

    Fix miscellanous issues with the progressive-image view

--- src/sculpture/admin/views/entity/partials/photos.cljs
+++ src/sculpture/admin/views/entity/partials/photos.cljs
@@ -17,36 +17,35 @@
     "http://placehold.it/50x50"))
 
 (defn progressive-image-view [_]
-  (let [canvas (r/atom nil)
-        actual-loaded? (r/atom false)
-        preview-loaded? (r/atom false)]
+  (let [canvas (r/atom nil)]
     (fn [{:keys [width height url-large url-preview color]}]
-      [:div {:ref (fn [node]
-                    (when (and node @canvas)
-                      ; setting canvas dimensions here (vs as attrs below)
-                      ; prevents a mysterious infinite loop
-                      (set! (.. @canvas -width) (.-offsetWidth node))
-                      (set! (.. @canvas -height) (.-offsetHeight node))))
-             :style {:width "100%"
-                     :padding-bottom (let [ratio (if (and width height)
-                                                   (* 100 (/ height width))
-                                                   100)]
-                                       (str ratio "%"))
-                     :position "relative"
-                     :background-color color}}
+      [:div.image
+       {:ref (fn [node]
+               (when (and node @canvas)
+                 ; setting canvas dimensions here (vs as attrs below)
+                 ; prevents a mysterious infinite loop
+                 (set! (.. @canvas -width) (.-offsetWidth node))
+                 (set! (.. @canvas -height) (.-offsetHeight node))))
+        :style {:overflow "hidden"
+                :width "100%"
+                :padding-bottom (let [ratio (if (and width height)
+                                               (* 100 (/ height width))
+                                               100)]
+                                  (str ratio "%"))
+                :position "relative"
+                :background-color color}}
+       ; if reagent redraws this component, the canvas is cleared
+       ; instead of setting opacity using reagent, it's done statefully via js
        [:canvas
         {:ref (fn [node]
-                (reset! canvas node))
-         :style {:position "absolute"
-                 :opacity (if @preview-loaded? 1 0)
+                (when (and node (not @canvas))
+                  (reset! canvas node)))
+         :style {:opacity 0
+                 :position "absolute"
                  :transition "opacity 0.25s ease-in-out"}}]
-       [:img {:ref (fn [node]
-                     ; setting the image src here (vs as an attr)
-                     ; prevents the "tainted src" error when figwheel reloads
-                     (when node
-                       (set! (.-src node) url-preview)))
-              :crossOrigin "Anonymous"
+       [:img {:crossOrigin "Anonymous"
               :style {:display "none"}
+              :src url-preview
               :on-load (fn [e]
                          (let [img (.. e -target)]
                            (.drawImage (.getContext @canvas "2d") img
@@ -57,31 +56,30 @@
                                                    0 0
                                                    (.-offsetWidth @canvas)
                                                    (.-offsetHeight @canvas)
-                                                   20)
-                           (reset! preview-loaded? true)))}]
+                                                   30)
+                           (set! (.. @canvas -style -opacity) 1)))}]
        [:img {:src url-large
-              :on-load (fn [_]
-                         (reset! actual-loaded? true))
-              :style {:opacity (if @actual-loaded? 1 0)
+              :on-load (fn [e]
+                         (set! (.. e -target -style -opacity) 1))
+              :style {:opacity 0
                       :position "absolute"
-                      :transition "opacity 1s ease-in-out"
+                      :transition "opacity 0.75s ease-in-out"
                       :width "100%"}}]])))
 
 (defn image-view [{:keys [photo size] :as args}]
   (if (= size :thumb)
-    [:img {:src (image-url photo size)}]
+    [:div.image
+     [:img {:src (image-url photo size)}]]
+    ^{:key (photo :id)}
     [progressive-image-view {:url-large (image-url photo size)
                              :url-preview (image-url photo :preload)
                              :color (get-in photo [:colors 0])
                              :width (photo :width)
                              :height (photo :height)}]))
 
-(defn photo-view [{:keys [photo size attribution? on-click]}]
+(defn photo-view [{:keys [photo size on-click]}]
   [:div.photo {:on-click (or on-click (fn []))}
    [image-view {:photo photo
-                :size size}]
-
-   (when attribution?
-     [:div "By ..."])])
+                :size size}]])
 
 
