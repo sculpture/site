d47e37dce328a57876250232defa5801b92d0360
Rafal Dittwald <rafal.dittwald@gmail.com>
2017-07-05 16:20:37 +0100

    Respond with UTF-8

--- src/sculpture/server/client_routes.clj
+++ src/sculpture/server/client_routes.clj
@@ -8,17 +8,17 @@
 (defroutes routes
 
   (GET  "/js/sculpture.js" _
-    (if-let [response (resource-response (str "public/js/sculpture.js"))]
+    (if-let [response (resource-response "public/js/sculpture.js")]
       (assoc-in response [:headers "Cache-Control"] "max-age=365000000, immutable")
       {:status 404
-       :headers {"Content-Type" "application/javascript"}
+       :headers {"Content-Type" "application/javascript; charset=utf-8"}
        :body (str "alert('JS files are missing. Please compile them with cljsbuild or figwheel.');")}))
 
   (resources "/")
 
   (GET "/*" _
     {:status 200
-     :headers {"Content-Type" "text/html"}
+     :headers {"Content-Type" "text/html; charset=utf-8"}
      :body (pages.index/html)}))
 
 (def handler
