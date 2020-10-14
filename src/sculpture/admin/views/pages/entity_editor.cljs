0ede255d6c8e2d8598d0def86cc36c1d309ff527
Rafal Dittwald <rafal.dittwald@gmail.com>
2020-10-13 22:35:06 -0400

    Fix error when closing entity-editor

--- src/sculpture/admin/views/pages/entity_editor.cljs
+++ src/sculpture/admin/views/pages/entity_editor.cljs
@@ -202,56 +202,57 @@
     {}))
 
 (defn entity-editor-view [entity]
-  (let [invalid-fields @(subscribe [:sculpture.edit/invalid-fields])
-        default-entity (entity-defaults (:type entity))]
-    [:div.entity.edit
-     [:div.header
-      [:h1 "Editing " (or (entity :name)
-                          (entity :title)
-                          (entity :slug)
-                          (entity :id))]
-      [:button.close
-       {:on-click (fn [_]
-                    (dispatch! [:sculpture.edit/stop-editing]))} "Close"]
+  (when entity
+    (let [invalid-fields @(subscribe [:sculpture.edit/invalid-fields])
+          default-entity (entity-defaults (:type entity))]
+      [:div.entity.edit
+       [:div.header
+        [:h1 "Editing " (or (entity :name)
+                            (entity :title)
+                            (entity :slug)
+                            (entity :id))]
+        [:button.close
+         {:on-click (fn [_]
+                      (dispatch! [:sculpture.edit/stop-editing]))} "Close"]
 
-      (let [saving? @(subscribe [:sculpture.edit/saving?])
-            invalid? (not (empty? invalid-fields))]
-        [:button.save {:disabled (or saving? invalid?)
-                       :class (when invalid? "invalid")
-                       :on-click
-                       (fn [_]
-                         (dispatch! [:sculpture.edit/save]))}
-         (cond
-           invalid?
-           "Invalid"
-           saving?
-           "Saving..."
-           :else
-           "Save")])]
+        (let [saving? @(subscribe [:sculpture.edit/saving?])
+              invalid? (not (empty? invalid-fields))]
+          [:button.save {:disabled (or saving? invalid?)
+                         :class (when invalid? "invalid")
+                         :on-click
+                         (fn [_]
+                           (dispatch! [:sculpture.edit/save]))}
+           (cond
+             invalid?
+             "Invalid"
+             saving?
+             "Saving..."
+             :else
+             "Save")])]
 
-     [:table
-      [:tbody
-       (for [k (keys default-entity)]
-         (let [v (or (entity k)
-                     (default-entity k))]
-           ^{:key k}
-           [:tr {:class (when (contains? invalid-fields k)
-                          "invalid")}
+       [:table
+        [:tbody
+         (for [k (keys default-entity)]
+           (let [v (or (entity k)
+                       (default-entity k))]
+             ^{:key k}
+             [:tr {:class (when (contains? invalid-fields k)
+                            "invalid")}
 
-            [:td.key (key->title k)]
+              [:td.key (key->title k)]
+              [:td
+               [field (merge
+                        (field-opts k (entity :type))
+                        {:value v
+                         :on-change (fn [v]
+                                      (dispatch! [:sculpture.edit/update-draft k v]))})]]
+              [:td [:button.delete
+                    {:on-click (fn []
+                                 (dispatch! [:sculpture.edit/remove-draft-key k]))}]]]))
+
+         (when (= "photo" (:type entity))
+           [:tr
+            [:td]
             [:td
-             [field (merge
-                      (field-opts k (entity :type))
-                      {:value v
-                       :on-change (fn [v]
-                                    (dispatch! [:sculpture.edit/update-draft k v]))})]]
-            [:td [:button.delete
-                  {:on-click (fn []
-                               (dispatch! [:sculpture.edit/remove-draft-key k]))}]]]))
-
-       (when (= "photo" (:type entity))
-         [:tr
-          [:td]
-          [:td
-           [:img {:src (cdn/image-url entity :thumb)}]]
-          [:td]])]]]))
+             [:img {:src (cdn/image-url entity :thumb)}]]
+            [:td]])]]])))
