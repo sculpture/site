4777bebff82c7468d891f3ec71324c1cdd330d21
Rafal Dittwald <rafal.dittwald@gmail.com>
2022-08-30 20:27:24 -0400

    Add some old import scripts

--- src/sculpture/scripts/misc.clj
+++ src/sculpture/scripts/misc.clj
@@ -1,10 +1,12 @@
 (ns sculpture.scripts.misc
   (:require
+    [bloom.commons.uuid :as uuid]
     [clojure.string :as string]
     [clojure.java.io :as io]
     [sculpture.db.core :as db]
     [sculpture.db.yaml :as yaml]
-    [sculpture.db.pg.select :as db.select]))
+    [sculpture.db.pg.select :as db.select]
+    [sculpture.db.pg.upsert :as db.upsert]))
 
 (def base-path "../sculpture-data/")
 
@@ -107,3 +109,141 @@
            (->> sculpture
                 (remove :city-id)
                 (map (juxt :title :id)))))
+
+(comment
+  ;; nationalities
+
+  ;; get from text on artist entities
+  (->> (io/file "../sculpture-data/data/artist")
+       file-seq
+       (filter (fn [file]
+                 (string/ends-with? (.getName file) ".yml")))
+       (map slurp)
+       (map yaml/from-string)
+       (map :nationality)
+       (filter string?)
+       (mapcat (fn [s]
+                 (string/split s #",| |-")))
+       set)
+
+  ;; export to files
+  (->> (for [[demonym nation] {"American" "United States of America"
+                               "Armenian" "Armenia"
+                               "Australian" "Australia"
+                               "Austrian" "Austria"
+                               "Belgian" "Belgium"
+                               "Bulgarian" "Bulgaria"
+                               "Canadian" "Canada"
+                               "Chilean" "Chile"
+                               "Chinese" "China"
+                               "Colombian" "Columbia"
+                               "Costa-Rican" "Costa-Rica"
+                               "Croatian" "Croatia"
+                               "Cuban" "Cuba"
+                               "Czech" "Chechia"
+                               "Danish" "Denmark"
+                               "Dutch" "Netherlands"
+                               "English" "England"
+                               "Finnish" "Finland"
+                               "French" "France"
+                               "German" "Germany"
+                               "Greek" "Greece"
+                               "Hungarian" "Hungary"
+                               "Icelandic" "Iceland"
+                               "Indian" "India"
+                               "Iranian" "Iran"
+                               "Iraqi" "Iraq"
+                               "Irish" "Ireland"
+                               "Israeli" "Israel"
+                               "Italian" "Italy"
+                               "Japanese" "Japan"
+                               "Jewish" nil
+                               "Korean" "Korea"
+                               "Latvian" "Latvia"
+                               "Lithuanian" "Lithuania"
+                               "Mexican" "Mexico"
+                               "Norwegian" "Norway"
+                               "Pakistani" "Pakistan"
+                               "Polish" "Poland"
+                               "Portuguese" "Portugal"
+                               "Romanian" "Romania"
+                               "Russian" "Russia"
+                               "Scottish" "Scotland"
+                               "Serbian" "Serbia"
+                               "Slovak" "Slovenia"
+                               "South-African" "South-Africa"
+                               "Spanish" "Spain"
+                               "Swedish" "Sweden"
+                               "Swiss" "Switzerland"
+                               "Taiwanese" "Taiwan"
+                               "Ukrainian" "Ukraine"
+                               "Venezuelan" "Venezuela"
+                               "Vietnamese" "Vietnam"
+                               "Welsh" "Wales"
+                               "Zimbabwean" "Zimbabwe"
+                               "British" "United Kingdom"
+                               "Flemish" "Flanders"}]
+         {:id (uuid/random)
+          :type "nationality"
+          :slug (slugify demonym)
+          :demonym demonym
+          :nation nation})
+       (map (partial db/save-to-file! "../sculpture-data"))
+       dorun)
+
+  ;; import from file
+  (->> (io/file "../sculpture-data/data/nationality")
+       file-seq
+       (filter (fn [file]
+                 (string/ends-with? (.getName file) ".yml")))
+       (map slurp)
+       (map yaml/from-string)
+       (map db.upsert/upsert-entity!)
+       dorun)
+
+  #_(db.upsert/upsert-entity! (yaml/from-string (slurp "../sculpture-data/data/nationality/47f58352-ea8b-4f2a-a6da-ceca7d19da75.yml")))
+  ;; modify artists
+  (let [nationalities (db.select/select-all-with-type "nationality")
+        nationality->id (zipmap
+                          (map :demonym nationalities)
+                          (map :id nationalities))]
+    (->> (io/file "../sculpture-data/data/artist")
+         file-seq
+         (filter (fn [file]
+                   (string/ends-with? (.getName file) ".yml")))
+         (map slurp)
+         (map yaml/from-string)
+         (map (fn [artist]
+                (assoc artist
+                  :nationality-ids
+                  (if-let [nstring (:nationality artist)]
+                    (for [n (string/split nstring #", | ")]
+                      (or (nationality->id n)
+                          (throw (ex-info (str "Can't find" (pr-str n) " for " (:name artist)) {}))))
+                    []))))
+         (map (partial db/save-to-file! "../sculpture-data"))
+         dorun))
+
+  ;; remove original nationality
+  (->> (io/file "../sculpture-data/data/artist")
+       file-seq
+       (filter (fn [file]
+                 (string/ends-with? (.getName file) ".yml")))
+       (map slurp)
+       (map yaml/from-string)
+       (map (fn [artist]
+              (dissoc artist :nationality)))
+       (map (partial db/save-to-file! "../sculpture-data"))
+       dorun)
+
+  ;; re-import artists
+
+  (->> (io/file "../sculpture-data/data/artist")
+       file-seq
+       (filter (fn [file]
+                 (string/ends-with? (.getName file) ".yml")))
+       (map slurp)
+       (map yaml/from-string)
+       (map db.upsert/upsert-entity!)
+       dorun)
+  )
