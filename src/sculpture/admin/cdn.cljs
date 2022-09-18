98d3e43aede58ae3dd8064857e5b68a7f3762ba0
Rafal Dittwald <rafal.dittwald@gmail.com>
2022-09-18 16:25:00 -0400

    Fix placeholder images

--- src/sculpture/admin/cdn.cljs
+++ src/sculpture/admin/cdn.cljs
@@ -5,11 +5,11 @@
    "https://1510904592.rsc.cdn77.org/")
 
 (defn image-url [{:photo/keys [id] :as photo} size]
-  (if photo
+  (if id
     (case (or size :thumb)
       :preload (str photo-host "preload/" id ".jpg")
       :thumb (str photo-host "thumb/" id ".jpg")
       :medium (str photo-host "medium/" id ".jpg")
       :large (str photo-host "large/" id ".jpg")
       :original (str photo-host "original/" id ".jpg"))
-    "http://placehold.it/50x50"))
+    "http://via.placeholder.com/50x50"))
