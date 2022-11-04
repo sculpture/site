82972429d9b9e3cb168fde8b889a6dee9d6fe9f4
Rafal Dittwald <rafal.dittwald@gmail.com>
2022-11-04 11:01:18 -0400

    Add pg_trgm extension

--- src/sculpture/db/pg/sql/init.sql
+++ src/sculpture/db/pg/sql/init.sql
@@ -4,6 +4,7 @@ BEGIN;
 
 --CREATE EXTENSION IF NOT EXISTS "postgis";
 --CREATE EXTENSION IF NOT EXISTS "postgis_topology";
+--CREATE EXTENSION IF NOT EXISTS "pg_trgm";
 
 CREATE TABLE IF NOT EXISTS cities (
   -- required:
