00356e4ce734e5f603419f4a260ec065c9e849af
Rafal Dittwald <rafal.dittwald@gmail.com>
2023-03-02 18:04:56 -0500

    Get rid of timeout hack in sidebar

--- src/sculpture/admin/views/sidebar/search.cljs
+++ src/sculpture/admin/views/sidebar/search.cljs
@@ -1,28 +1,34 @@
 (ns sculpture.admin.views.sidebar.search
   (:require
+    [reagent.core :as r]
     [clojure.string :as string]
     [sculpture.admin.state.api :refer [subscribe dispatch!]]
     [sculpture.admin.views.sidebar.entity.partials.list :refer [row-view]]))
 
 (defn query-view []
-  (let [query @(subscribe [:state.search/query])]
-    [:div.query
-     [:input {:placeholder "Search Sculpture"
-              :default-value query
-              :auto-focus true
-              :on-focus (fn [_]
-                          (dispatch! [:state.search/set-query-focused! true]))
-              :on-blur (fn [e]
-                         ; set-page does a set-query-focused anyway, so, this is mostly relevant for tabbing
-                         ; but, need to have a timeout, b/c otherwise, when user clicks a search result
-                         ; the on-blur removes the link before the on-click happens
-                         (js/setTimeout (fn [] (dispatch! [:state.search/set-query-focused! false])) 250))
-              :on-change (fn [e]
-                           (dispatch! [:state.search/set-query! (.. e -target -value)]))}]
-     (when (seq query)
-       [:button.clear
-        {:on-click (fn [_]
-                     (dispatch! [:state.search/set-query! ""]))}])]))
+  (r/with-let [;; with on-blur, when a user clicks a search result
+               ;; on-blur removes the link before the on-click happens
+               ;; this pseudo-on-blur happens after the on-click (bubbles to window)
+               pseudo-on-blur (fn [e]
+                                (when (not= (.. e -target -id) "search")
+                                  (dispatch! [:state.search/set-query-focused! false])))
+               _ (..  js/window (addEventListener "click" pseudo-on-blur false))]
+    (let [query @(subscribe [:state.search/query])]
+      [:div.query
+       [:input {:placeholder "Search Sculpture"
+                :default-value query
+                :auto-focus true
+                :id "search"
+                :on-focus (fn [_]
+                            (dispatch! [:state.search/set-query-focused! true]))
+                :on-change (fn [e]
+                             (dispatch! [:state.search/set-query! (.. e -target -value)]))}]
+       (when (seq query)
+         [:button.clear
+          {:on-click (fn [_]
+                       (dispatch! [:state.search/set-query! ""]))}])])
+    (finally
+      (.. js/window (removeEventListener "click" pseudo-on-blur false)))))
 
 (defn results-view []
   [:div.results
